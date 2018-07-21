83cf88bf3f8d002df5f11858d8453ee40ccfef09
Sangeeth Sudheer <sangeethmanayil@outlook.com>
2018-07-21 06:12:32 +0530

    Persist theme setting using localStorage

--- docs/theme/theme.js
+++ docs/theme/theme.js
@@ -1,20 +1,54 @@
-var $body = document.body;
-var $themeBtns = document.querySelectorAll(".js-theme-button");
+(function() {
+  var body = document.body;
 
-$themeBtns[0].setAttribute("disabled", true);
+  var themeBtns = document.querySelectorAll(".js-theme-button");
+  var themeList = document.querySelector(".js-theme-list");
+  var themeNames = [].map.call(themeBtns, el => el.value);
 
-$themeBtns.forEach(function(ele) {
-  ele.onclick = function(e) {
-    var selectedName = e.target.value;
+  var currentBtn = document.querySelector(".js-theme-button:disabled");
+  var newBtn;
 
-    $themeBtns.forEach(function(ele) {
-      var themeName = ele.value;
-      if (themeName === selectedName) return;
-      $body.classList.remove("js-theme-" + themeName);
-      ele.removeAttribute("disabled");
-    });
+  var userTheme = localStorage.getItem("theme");
 
-    $body.classList.add("js-theme-" + selectedName);
-    ele.setAttribute("disabled", true);
-  };
-});
+  if (userTheme && themeNames.indexOf(userTheme) != -1) {
+    newBtn = document.querySelector(
+      '.js-theme-button[value="' + userTheme + '"]'
+    );
+
+    changeTheme(currentBtn.value, userTheme);
+    updateBtns(currentBtn, newBtn);
+  } else {
+    currentBtn = themeBtns[0];
+  }
+
+  themeList.addEventListener("click", function(e) {
+    var themeName;
+
+    if (e.target.nodeType === 1) {
+      // Theme already active
+      if (e.target.disabled) return;
+
+      themeName = e.target.value;
+
+      changeTheme(currentBtn.value, themeName);
+      updateBtns(currentBtn, e.target);
+      storeTheme(themeName);
+    }
+  });
+
+  function changeTheme(oldTheme, newTheme) {
+    body.classList.add("js-theme-" + newTheme);
+    body.classList.remove("js-theme-" + oldTheme);
+  }
+
+  function storeTheme(themeName) {
+    localStorage.setItem("theme", themeName);
+  }
+
+  function updateBtns(prevBtn, newBtn) {
+    prevBtn.disabled = false;
+    newBtn.disabled = true;
+
+    currentBtn = newBtn;
+  }
+})();
