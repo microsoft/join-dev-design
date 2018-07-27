5833c8469a6a2f26756673526ed0d286358b610f
Ali Nagi <anagi202@gmail.com>
2018-07-27 04:04:47 -0400

    Simple One Button Solution

--- docs/theme/theme.js
+++ docs/theme/theme.js
@@ -1,8 +1,11 @@
 var $body = document.body;
+var switchButton = document.getElementById("switchTheme");
 var activeTheme = "dark";
 
 document.querySelector(".theme").onclick = function(e) {
   $body.classList.remove("js-theme-" + activeTheme);
+  switchButton.classList.remove(activeTheme);
   activeTheme = activeTheme === "dark" ? "light" : "dark";
   $body.classList.add("js-theme-" + activeTheme);
+  switchButton.classList.add(activeTheme);
 };
