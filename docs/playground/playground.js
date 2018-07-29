f8946e0e6b62ce4c335aeeba79a26cc3501feacc
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2018-07-29 00:25:14 -0700

    add zoom in animation

--- docs/playground/playground.js
+++ docs/playground/playground.js
@@ -1,10 +1,13 @@
+var sidePanel = document.getElementById("side-panel");
 var sidePushableContent = document.getElementById("side-pushable-content");
 
 document.getElementById("toggle-side-panel-open").onclick = function() {
+  sidePanel.classList.add("side-panel-open");
   sidePushableContent.classList.add("side-pushable-content-open");
 };
 
 document.getElementById("side-pushable-content").onclick = function(e) {
   if (e.target.id === "toggle-side-panel-open") return;
+  sidePanel.classList.remove("side-panel-open");
   sidePushableContent.classList.remove("side-pushable-content-open");
 };
