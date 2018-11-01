2d03191e9c28d0e0fe232e3d247ea5fd16eb6024
Louis-Michel Couture <louismichel.couture@hooktstudios.com>
2018-10-31 22:07:09 -0400

    Run the build script to add missing history

---
+++ docs/time-travel/history/MDExOlB1bGxSZXF1ZXN0MjA0NDk3OTYx/docs/theme/theme.js
@@ -0,0 +1,44 @@
+const USER_THEME = "ms-tt-user-theme";
+const storedTheme = window.ls.getItem(USER_THEME);
+
+var theme = {
+  activeTheme: "dark",
+  load: function(theme, cb) {
+    var id = "theme-" + theme;
+    if (!document.getElementById(id)) {
+      var $head = document.getElementsByTagName("head")[0];
+      var $link = document.createElement("link");
+      $link.id = id;
+      $link.rel = "stylesheet";
+      $link.type = "text/css";
+      $link.href = "./theme/" + theme + "/theme.css";
+      $head.appendChild($link);
+
+      $link.onload = function() {
+        if (!!cb) cb();
+      };
+      $link.onerror = function() {
+        console.warn("loading " + theme + " theme failed.");
+      };
+    }
+  },
+  changeTo: function(theme) {
+    if (["light", "dark"].indexOf(theme) === -1) {
+      this.load(theme);
+    } else {
+      window.ls.setItem(USER_THEME, theme);
+    }
+    document.body.classList.remove("js-theme-" + this.activeTheme);
+    this.activeTheme = theme;
+    document.body.classList.add("js-theme-" + this.activeTheme);
+  }
+};
+
+document.querySelector(".theme").onclick = function(e) {
+  var nextTheme = theme.activeTheme === "dark" ? "light" : "dark";
+  theme.changeTo(nextTheme);
+};
+
+if (storedTheme) {
+  theme.changeTo(storedTheme);
+}
